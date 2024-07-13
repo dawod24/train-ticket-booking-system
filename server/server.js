713e79d24cfd2a1158c18839922f58019da104a8
Dawod <modawodh@gmail.com>
2024-07-13 21:10:17 +0100

    modified server.js

--- server/server.js
+++ server/server.js
@@ -9,11 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// This line attempts to connect to your MongoDB database
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    // If the connection is successful, this part will run
+mongoose.connect(process.env.DATABASE_URL)
     .then(() => console.log('Connected to Database'))
-    // If there's an error in connecting, this part will run
     .catch(err => console.error('Could not connect to Database', err));
 
 const trainRoutes = require('./routes/trains');
