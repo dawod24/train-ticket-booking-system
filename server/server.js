93706f8308f2c70552b9ee101257aa91e06409af
Dawod <modawodh@gmail.com>
2024-07-13 20:35:22 +0100

    adjusted the server.js file

--- server/server.js
+++ server/server.js
@@ -9,8 +9,14 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.DATABASE_URL)
+// First, make sure you've imported mongoose at the top of your file
+const mongoose = require('mongoose');
+
+// This line attempts to connect to your MongoDB database
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    // If the connection is successful, this part will run
     .then(() => console.log('Connected to Database'))
+    // If there's an error in connecting, this part will run
     .catch(err => console.error('Could not connect to Database', err));
 
 const trainRoutes = require('./routes/trains');
